ae503f457b5ca2971631766998b8c73ad4eef7b4
Rob <robmuir1@gmail.com>
2026-07-09 11:52:09 -0400

    workflows

--- fabex/tests/install_addon.py
+++ fabex/tests/install_addon.py
@@ -7,19 +7,19 @@ import tempfile
 # from fabex.tests.constants import INSTALL_CODE, NUM_RETRIES, BLENDER as blender
 # from fabex.tests.base import blender_command
 
-INSTALL_CODE = f"""
-import bpy
-bpy.context.preferences.system.use_online_access = True
-bpy.ops.extensions.repo_sync_all(use_active_only=False)
-bpy.ops.extensions.package_install(repo_index=0, pkg_id="stl_format_legacy")
-bpy.ops.extensions.package_install(repo_index=0, pkg_id="simplify_curves_plus")
-bpy.ops.extensions.package_install(repo_index=0, pkg_id="curve_tools")
-bpy.ops.extensions.package_install_files(filepath='{sys.argv[1]}', repo='user_default')
-bpy.ops.wm.save_userpref()
-bpy.ops.wm.read_homefile(app_template="")
-bpy.ops.script.reload()
-bpy.ops.wm.quit_blender()
-"""
+# INSTALL_CODE = f"""
+# import bpy
+# bpy.context.preferences.system.use_online_access = True
+# bpy.ops.extensions.repo_sync_all(use_active_only=False)
+# bpy.ops.extensions.package_install(repo_index=0, pkg_id="stl_format_legacy")
+# bpy.ops.extensions.package_install(repo_index=0, pkg_id="simplify_curves_plus")
+# bpy.ops.extensions.package_install(repo_index=0, pkg_id="curve_tools")
+# bpy.ops.extensions.package_install_files(filepath='{sys.argv[1]}', repo='user_default')
+# bpy.ops.wm.save_userpref()
+# bpy.ops.wm.read_homefile(app_template="")
+# bpy.ops.script.reload()
+# bpy.ops.wm.quit_blender()
+# """
 
 
 NUM_RETRIES = 10
@@ -49,20 +49,20 @@ NUM_RETRIES = 10
 #                     print(crash_file.read_text())
 #                     print("============================")
 
-# INSTALL_CODE = """
-# from pathlib import Path
+INSTALL_CODE = """
+from pathlib import Path
 
-# import bpy
+import bpy
 
-# version_file = Path(__file__).parent.parent / "version.py"
-# with open(version_file) as f:
-#     lines = f.readlines()
-#     version = lines[0].split("(")[1].replace(",", "")
-# major, minor, patch = version[0], version[1], version[2]
-# path = str(Path(__file__).parent.parent.parent / f"fabex-{major}.{minor}.{patch}.zip")
-# bpy.ops.extensions.package_install_files(filepath=path, repo="user_default")
+version_file = Path(__file__).parent.parent / "version.py"
+with open(version_file) as f:
+    lines = f.readlines()
+    version = lines[0].split("(")[1].replace(",", "")
+major, minor, patch = version[0], version[1], version[2]
+path = str(Path(__file__).parent.parent.parent / f"fabex-{major}.{minor}.{patch}.zip")
+bpy.ops.extensions.package_install_files(filepath=path, repo="user_default")
 
-# """
+"""
 
 
 with tempfile.TemporaryDirectory() as td:
